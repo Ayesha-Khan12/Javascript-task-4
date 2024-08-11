acb677c06256dd38058441e058e69bde7cfc5e3a
Ayesha-Khan12 <developerayesha582@gmail.com>
2024-08-12 00:19:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+var heading = "<h2>Rules for naming JS variables</h2>";
+document.write(heading);
+var number = "numbers";
+var $dollar = "$";
+var underScore = "_.";
+document.write("Variable name can only contain ," + " " + number + "," + " " + "and" + " " + underScore + " " + "For example : $my_1stVariable" );
+
+var _rule2="<br><br>Variables must begin with a letter , $ or _ . For example $name, _name or name .";
+document.write(_rule2);
+
+var $rule3="<br><br>Variable names are case sensitive .";
+document.write($rule3);
+
+var rule_4="<br><br>Variable names should not be JS keywords . ";
+document.write(rule_4);
+
+
+
+
+
+
